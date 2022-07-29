f999b62bb79954a2ac7adf096eede93aacd7bf30
Chriserus <krzysborowiecki97@gmail.com>
2022-07-29 09:22:57 +0200

    Add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TextField} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <TextField label="Outlined" variant="outlined" />
         <a
           className="App-link"
           href="https://reactjs.org"
