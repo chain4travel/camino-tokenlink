d4b225ec76e1715d8e28281fa76b58177032b064
Chriserus <krzysborowiecki97@gmail.com>
2022-08-02 09:37:40 +0200

    Connect via web3modal, hotswap errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,81 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {TextField} from "@mui/material";
-import {InjectedConnector} from "@web3-react/injected-connector";
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+import WalletConnect from "@walletconnect/web3-provider";
+import Web3Modal from 'web3modal';
+import {ethers, providers} from "ethers";
+import coinlink from '@coinlink/contracts/Coinlink.json';
 
-import {useWeb3React} from '@web3-react/core'
+export const providerOptions = {
+    coinbasewallet: {
+        package: CoinbaseWalletSDK,
+        options: {
+            appName: "Web 3 Modal Demo",
+            infuraId: process.env.REACT_APP_INFURA_KEY
+        }
+    },
+    walletconnect: {
+        package: WalletConnect,
+        options: {
+            infuraId: process.env.REACT_APP_INFURA_KEY
+        }
+    }
+};
 
-const Injected = new InjectedConnector({
-    supportedChainIds: [1337]
-});
+let networkId = '5777';
+export type Networks = Partial<Record<string, { address: string }>>;
 
 function App() {
-    const {activate, deactivate} = useWeb3React();
-    const {active, chainId, account} = useWeb3React();
+    const [provider, setProvider] = useState();
+    const [, setLibrary] = useState({});
+    const [account, setAccount] = useState('');
+    const [, setNetwork] = useState({});
+    const web3Modal = new Web3Modal({
+        cacheProvider: true,
+        providerOptions
+    });
+    const connectWallet = async () => {
+        try {
+            const provider = await web3Modal.connect();
+            const library = new ethers.providers.Web3Provider(provider);
+            const accounts = await library.listAccounts();
+            const network = await library.getNetwork();
+            setProvider(provider);
+            setLibrary(library);
+            if (accounts) setAccount(accounts[0]);
+            setNetwork(network);
+            console.log(provider);
+            console.log(library);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const contractInteraction = async () => {
+        if (!provider) return;
+        const ethersProvider = new providers.Web3Provider(provider);
+        try {
+            const contract = new ethers.Contract((coinlink.networks as Networks)[networkId]?.address as string, coinlink.abi, ethersProvider);
+            console.log(contract);
+            const result = await contract.initialAmount();
+            console.log(ethers.utils.formatEther(result));
+            console.log('change');
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <TextField label="Outlined" variant="outlined"/>
-                <button onClick={() => {
-                    activate(Injected)
-                }}>Metamask
-                </button>
-                <div>Connection Status: {active ? 'True' : 'False'}</div>
-                <div>Account: {account}</div>
-                <div>Network ID: {chainId}</div>
-                <button onClick={deactivate}>Disconnect</button>
+                <button onClick={connectWallet}>Connect Wallet</button>
+                <div>Connection Status: {!!account ? 'True' : 'False'}</div>
+                <div>Wallet Address: {account}</div>
+                <button onClick={contractInteraction}>Interact with Contract</button>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Web3ReactProvider} from '@web3-react/core'
-import {ExternalProvider, JsonRpcFetchFunc, Web3Provider} from "@ethersproject/providers";
-
-function getLibrary(provider: ExternalProvider | JsonRpcFetchFunc) {
-    return new Web3Provider(provider);
-}
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <Web3ReactProvider getLibrary={getLibrary}>
-            <App/>
-        </Web3ReactProvider>
+        <App/>
     </React.StrictMode>
 );
 
