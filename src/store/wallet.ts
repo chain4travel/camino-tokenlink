3c08f4c9f13dbda3986a575988bb40d04b1fcdd0
aeddaqqa <achrafeddaqqaq@gmail.com>
2022-09-12 10:46:11 +0100

    add selectors for wallet slice

--- src/store/wallet.ts
+++ src/store/wallet.ts
@@ -1,12 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { ethers, providers } from "ethers";
-import Web3Modal from "web3modal";
-import WalletConnect from "@walletconnect/web3-provider";
-// import Web3Modal from "web3modal";
-import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
-import { connectWeb3 } from "./utils";
 import { RootState } from ".";
-// import { RootState } from "./index";
 
 export interface AdminPanelProps {
   coinlinks: any[];
@@ -36,30 +29,15 @@ const walletSlice = createSlice({
       state.initialAmount = action.payload;
     },
   },
-  extraReducers(builder) {
-    builder.addCase(connectWeb3.pending, (state, action) => {
-      console.log(action);
-    });
-    builder.addCase(
-      connectWeb3.fulfilled,
-      (state, { payload: { provider, signer, account } }) => {
-        // state.account = account;
-        // state.provider = provider;
-        // state.signer = signer;
-      }
-    );
-    builder.addCase(connectWeb3.rejected, (state, action) => {
-      console.log(action);
-    });
-  },
 });
 
-// Select Signer
-export const getSigner = (state: RootState) => state.web3Resources.signer;
-// Select Provider
-export const getProvider = (state: RootState) => state.web3Resources.provider;
-// Select Account
-export const getAccount = (state: RootState) => state.web3Resources.account;
+// Select Balance
+export const getBalance = (state: RootState) => state.wallet.balance;
+// Select CoinLinks
+export const getCoinLinks = (state: RootState) => state.wallet.coinlinks;
+// Select InitialAmount
+export const getInitialAmount = (state: RootState) =>
+  state.wallet.initialAmount;
 
 export const { setBalance, setCoinLinks, setInitialAmount } =
   walletSlice.actions;
